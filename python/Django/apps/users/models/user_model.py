42f93b21b485a521529d2200f9098b6edf7f7710
Masato Nitta <newmtube07@gmail.com>
2025-10-27 18:28:40 +0900

    update

--- python/Django/apps/users/models/user_model.py
+++ python/Django/apps/users/models/user_model.py
@@ -1,10 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
+class User(AbstractUser):
+
+    # AbstractUserを継承することで、Djangoの認証システムと互換性を持たせます。
 
     def __str__(self):
         return self.username
